a2b422f63130d0250205723cf4d9f0c931981599
Yash Pathik <yashpathik.official@gmail.com>
2025-12-16 23:27:20 +0530

    Initialize React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+]);
 
 function App() {
-    return (
-        <>
-            <h1>hello</h1>
-        </>
-    );
+    return <RouterProvider router={router} />;
 }
 
 export default App;
